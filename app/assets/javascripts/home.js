ed1b9c994d0b5c8ddbef2ffaaef45074b2ef09da
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-04-04 14:42:45 +0100

    moving javascript from home to application js. Using userInputs variable to get the values that came from the server

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,14 +1,2 @@
 $(function() {
-  $( "#source-slider" ).slider({
-    range: "min",
-    min: 0,
-    max: 100,
-    value: 81,
-    slide: function( event, ui ) {
-      $( "#source-percentage" ).text(ui.value + "%");
-      $("#total-from-groundwater").text($("#total-water").data('value') * (ui.value/100));
-    }
-  });
-  $( "#source-percentage" ).text($('#source-slider').slider('value') + "%");
-  $("#total-from-groundwater").text($("#total-water").data('value') * ($("#source-slider").slider('value')/100));
 });
