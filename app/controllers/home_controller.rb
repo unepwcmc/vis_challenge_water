5ff659919bed4f8833c6024c7938f340e0318bd9
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-04-04 12:26:05 +0100

    Graph working, and json being returned

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def defaults
+    render json: {
+      '2012' => { # current year
+        'groundwater_level' => 130_000_000_000,
+        'water_consumption_per_year' => 2_600_000_000
+      }
+    }
+  end
 end
