d0d8909ec30040713fb1bee56c23908bab7bb1a6
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-04-04 11:26:07 +0100

    created the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VisChallengeWater::Application.config.secret_token = 'dce6c9bb3cc7041e2701122bc667ae35369efc01f77b6613fdc1ee9c2fa5e16522f27ad62e00c5adf2a1f2ba24e5f0650282a1dbb1a60137017bc465a0d0d804'
