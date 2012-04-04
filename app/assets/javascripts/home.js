f93d00598bd808e98012b15379d6429ca06d8dae
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-04-04 13:48:56 +0100

    displayng some values, from the total water consumption

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,7 +6,9 @@ $(function() {
     values: [81],
     slide: function( event, ui ) {
       $( "#source-percentage" ).text(ui.values[0] + "%");
+      $("#total-from-groundwater").text($("#total-water").data('value') * (ui.values[0]/100));
     }
   });
   $( "#source-percentage" ).text($('#source-slider').slider('values', 0) + "%");
+  $("#total-from-groundwater").text($("#total-water").data('value') * ($("#source-slider").slider('values', 0)/100));
 });
